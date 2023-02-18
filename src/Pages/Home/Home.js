e007dee5e7a1aeaab672ba5fdf827ce367709518
Shahriar Ahmed <dipto2041@gmail.com>
2023-02-18 18:03:22 +0600

    reviews added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Banner from "./Banner";
+import Info from "./Info";
+import MakeApppointment from "./MakeApppointment";
+import Services from "./Services";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
   return (
-    <div>
+    <div className="px-12">
       <Banner></Banner>
+      <Info></Info>
+      <Services></Services>
+      <MakeApppointment></MakeApppointment>
+      <Testimonials></Testimonials>
     </div>
   );
 };
---
+++ src/Pages/Home/MakeApppointment.js
@@ -0,0 +1,31 @@
+import React from "react";
+import doctor from "../../assets/images/doctor.png";
+import appointment from "../../assets/images/appointment.png";
+import PrimaryButton from "../Shared/PrimaryButton";
+const MakeApppointment = () => {
+  return (
+    <section
+      style={{
+        background: `url(${appointment})`,
+      }}
+      className="flex justify-center items-center"
+    >
+      <div className="flex-1">
+        <img className="mt-[-100px] hidden lg:block" src={doctor} alt="doctor" />
+      </div>
+      <div className="flex-1">
+        <h3 className="text-xl text-primary font-bold">Appointment</h3>
+        <h1 className="text-3xl text-white">Make an appointment today</h1>
+        <p className="text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, alias
+          obcaecati? Repudiandae odio at totam ullam impedit quam pariatur,
+          culpa eius eum repellat, sequi inventore laboriosam fugiat? Adipisci
+          aperiam minus incidunt.
+        </p>
+        <PrimaryButton>Get Started</PrimaryButton>
+      </div>
+    </section>
+  );
+};
+
+export default MakeApppointment;
